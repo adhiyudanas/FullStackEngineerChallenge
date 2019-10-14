79fe20c9f2798f526eb96d11dd75f6150954e668
adhiyudanas <adhiyudanas@gmail.com>
2019-10-15 00:27:07 +0900

    finishing

--- database/sql-scripts/003-create_employees.sql
+++ database/sql-scripts/003-create_employees.sql
@@ -9,6 +9,5 @@ CREATE TABLE `employees` (
  `name`             varchar(128) NOT NULL,
  `bod`              varchar(128) NOT NULL,
  `department`       varchar(128) NOT NULL,
- PRIMARY KEY (`user_id`),
- FOREIGN KEY `fk_user_id` (`user_id`) REFERENCES users (`id`)
+ PRIMARY KEY (`user_id`), FOREIGN KEY `fk_user_id` (`user_id`) REFERENCES users (`id`)
 ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
--- database/sql-scripts/004-create_reviews.sql
+++ database/sql-scripts/004-create_reviews.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `reviews`;
 
 -- We will be using sampe vue component for viewing each employee activities, including reviews and feedback
 
-create table `reviews` (
+CREATE TABLE `reviews` (
  `id`               int(11) AUTO_INCREMENT,
  `created_at`       datetime DEFAULT NULL,
  `updated_at`       datetime DEFAULT NULL,
@@ -10,7 +10,6 @@ create table `reviews` (
  `review_content`   varchar(128) NOT NULL,
  `reviewer_user_id` varchar(128) NOT NULL,
  `feedback_content` varchar(128) NOT NULL,
- PRIMARY KEY (`id`),
- FOREIGN KEY `fk_target_user_id` (`target_user_id`) REFERENCES users (`id`),
+ PRIMARY KEY (`id`), FOREIGN KEY `fk_target_user_id` (`target_user_id`) REFERENCES users (`id`),
  FOREIGN KEY `fk_reviewer_user_id` (`reviewer_user_id`) REFERENCES users (`id`)
 ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
